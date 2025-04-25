233a1354f08526269bf036a8133060e28d6fc0bd
AngeloRipamonti <ripamontiangelo@itis-molinari.eu>
2025-04-25 12:09:13 +0200

    text to varchar

--- src/components/database.js
+++ src/components/database.js
@@ -22,16 +22,16 @@ module.exports = function database() {
 
             await Promise.all([
                 db.execute(`CREATE TABLE IF NOT EXISTS users(
-                            email TEXT PRIMARY KEY,
+                            email VARCHAR(255) PRIMARY KEY,
                             password TEXT NOT NULL,
                             password_salt TEXT NOT NULL,
-                            username TEXT NOT NULL,
+                            username VARCHAR(255) NOT NULL,
                             bio TEXT,
                             path_thumbnail TEXT DEFAULT '/assets/images/default_profile.png'
                         );`),
                 db.execute(`CREATE TABLE IF NOT EXISTS follows_users(
-                            email_parent TEXT,
-                            email_child TEXT,
+                            email_parent VARCHAR(255),
+                            email_child VARCHAR(255),
                             PRIMARY KEY (email_parent, email_child),
                             CONSTRAINT fk_parent FOREIGN KEY (email_parent) REFERENCES users(email)
                                 ON DELETE CASCADE
@@ -45,17 +45,17 @@ module.exports = function database() {
                             path_note TEXT,
                             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                             visibility BOOL DEFAULT false,
-                            author_email TEXT NOT NULL,
+                            author_email VARCHAR(255) NOT NULL,
                             CONSTRAINT fk_notes_author FOREIGN KEY (author_email) REFERENCES users(email)
                                 ON DELETE RESTRICT
                                 ON UPDATE CASCADE
                         );`),
                 db.execute(`CREATE TABLE IF NOT EXISTS tags(
-                            name TEXT PRIMARY KEY
+                            name VARCHAR(255) PRIMARY KEY
                         );`),
                 db.execute(`CREATE TABLE IF NOT EXISTS notes_tags(
                             id INT,
-                            name TEXT,
+                            name VARCHAR(255),
                             PRIMARY KEY (id, name),
                             CONSTRAINT fk_notes_tags_id FOREIGN KEY (id) REFERENCES notes(id)
                                 ON DELETE CASCADE
@@ -65,7 +65,7 @@ module.exports = function database() {
                                 ON UPDATE CASCADE
                         );`),
                 db.execute(`CREATE TABLE IF NOT EXISTS notes_edit(
-                            author_email TEXT,
+                            author_email VARCHAR(255),
                             id INT,
                             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                             PRIMARY KEY (author_email, id),
@@ -77,7 +77,7 @@ module.exports = function database() {
                                 ON UPDATE CASCADE
                         );`),
                 db.execute(`CREATE TABLE IF NOT EXISTS feedbacks(
-                            author_email TEXT,
+                            author_email VARCHAR(255),
                             id INT,
                             n_star INT NOT NULL,
                             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
