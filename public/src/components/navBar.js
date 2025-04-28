71df691353858417c93fdacb3b20fd78f8a8895f
Luca Avveduto <avvedutoluca@itis-molinari.eu>
2025-04-28 10:44:53 +0200

    nav-burger

--- public/src/components/navBar.js
+++ public/src/components/navBar.js
@@ -1,42 +1,40 @@
 export function generateNavbar(parentElement, pubsub) {
   const elements = {
-    "creation": {
-      "logo": true,
-      "right": [
-          '<img src="/assets/images/favicon.ico" id="logo">',
-          "<h3>Mind Sharing</h3>"
+    creation: {
+      logo: true,
+      right: [
+        '<img src="/assets/images/favicon.ico" id="logo">',
+        "<h3>Mind Sharing</h3>",
       ],
-      "left": [
-        "<h3>Document Creation</h3>"
-      ]
+      left: ["<h3>Document Creation</h3>"],
     },
-    "personal": {
-      "logo": false,
-      "right": [
+    personal: {
+      logo: false,
+      right: [
         "<button class='button is-white u-icon'>User Icon</button>",
         "<h3>$Username</h3>",
       ],
-      "left": [
-         "<button class='button is-white' id='u-settings'> <i class='fa fa-gear'></i> </button>",
-         "<button class='button is-rounded' id='doc-creation'>+</button>"
-      ]
+      left: [
+        "<button class='button is-white' id='u-settings'> <i class='fa fa-gear'></i> </button>",
+        "<button class='button is-rounded' id='doc-creation'>+</button>",
+      ],
     },
-    "accounts": {
-      "logo": false,
-      "right": [
-          "<button class='button is-white' id='acc-icon'>User Icon</button>",
-          "<h3>$Username</h3>",
-        ],
-      "left": ["<button class='button is-rounded'>Follow</button>"]
+    accounts: {
+      logo: false,
+      right: [
+        "<button class='button is-white' id='acc-icon'>User Icon</button>",
+        "<h3>$Username</h3>",
+      ],
+      left: ["<button class='button is-rounded'>Follow</button>"],
     },
-    "feed": {
-      "logo": true,
-      "right":  [
+    feed: {
+      logo: true,
+      right: [
         '<img src="/assets/images/favicon.ico" id="logo">',
-          "<h3>Mind Sharing</h3>"
-        ],
-        "left": [
-          `<p class="control has-icons-left has-icons-right">
+        "<h3>Mind Sharing</h3>",
+      ],
+      left: [
+        `<p class="control has-icons-left has-icons-right">
                 <input class="input" type="text" placeholder="Search for notes..." id="$searchBar">
                 <span class="icon is-left clickableIcon" id="$search-icon">
                     <i class="fas fa-search"></i>
@@ -46,14 +44,12 @@ export function generateNavbar(parentElement, pubsub) {
                 </span>
             </p>`,
         "<button class='button is-white u-icon'>User Icon</button>",
-      ]
-    },
-    "entry": {
-      "logo": true,
-      "right": [
-        '<img src="/assets/images/favicon.ico" id="logo">'
       ],
-      "left": ["<button class='button is-white' id='register'>Sign Up</button>"]
+    },
+    entry: {
+      logo: true,
+      right: ['<img src="/assets/images/favicon.ico" id="logo">'],
+      left: ["<button class='button is-white' id='register'>Sign Up</button>"],
     },
   };
 
@@ -67,56 +63,68 @@ export function generateNavbar(parentElement, pubsub) {
        </div>
     `;
 
-    const startTemplate = `
+  const startTemplate = `
        <div class="navbar-item">
           %content%
        </div>
     `;
 
-    function build(element) {
-      if (elements[element]) index = element;
-    }
+  function build(element) {
+    if (elements[element]) index = element;
+  }
 
   return {
-    render: function() {
-      console.log(index);
+    render: function () {
       const data = elements[index];
       parentElement.innerHTML = `
-              <nav class="navbar is-transparent">
-                  <div class="navbar-brand">
-                      <div class="navbar-burger js-burger" data-target="navbarExampleTransparentExample">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                      </div>
-                  </div>
-                  <div id="navbarExampleTransparentExample" class="navbar-menu">
-                      <div class="navbar-start">
-                          ${data.right.map((e) => {
-                              return startTemplate.replace("%content%", e);
-                            })
-                            .join("")}
-                      </div>
-                      
-                      <div class="navbar-end">
-                          ${data.left.map((e) => {
-                              return endTemplate.replace("%content%", e);
-                            })
-                            .join("")}
-                      </div>
-                  </div>
+              <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                  <a role="button" class="navbar-burger" data-target="navbarExampleTransparentExample" aria-label="menu" aria-expanded="false">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                  </a>
+                </div>
+                <div id="navbarExampleTransparentExample" class="navbar-menu">
+                    <div class="navbar-start">
+                        ${data.right
+                          .map((e) => {
+                            return startTemplate.replace("%content%", e);
+                          })
+                          .join("")}
+                    </div>
+                    <div class="navbar-end">
+                        ${data.left
+                          .map((e) => {
+                            return endTemplate.replace("%content%", e);
+                          })
+                          .join("")}
+                    </div>
+                </div>
               </nav>
           `;
-          
-           document.getElementById("register").onclick = () => {
-            pubsub.publish("sign-up");
-           }
 
-          pubsub.subscribe("newHash", (page) => {
-              build(page);
-              this.render();
-          });
+      document.getElementById("register").onclick = () => {
+        pubsub.publish("sign-up");
+      };
+
+      pubsub.subscribe("newHash", (page) => {
+        build(page);
+        this.render();
+      });
+
+      const navbarBurgers = Array.prototype.slice.call(
+        document.querySelectorAll(".navbar-burger"),
+        0
+      );
+      navbarBurgers.forEach((el) => {
+        el.addEventListener("click", () => {
+          const target = document.getElementById(el.dataset.target);
+          el.classList.toggle("is-active");
+          target.classList.toggle("is-active");
+        });
+      });
     },
   };
 }
