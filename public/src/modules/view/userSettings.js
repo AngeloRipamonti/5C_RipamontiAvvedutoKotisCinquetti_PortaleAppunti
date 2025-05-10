725393040aa230a412c3b0076e00c0040faa7ca5
Simone Cinquetti <cinquettisimone@itis-molinari.eu>
2025-05-10 12:13:46 +0200

    grafica user settings

--- public/src/modules/view/userSettings.js
+++ public/src/modules/view/userSettings.js
@@ -6,7 +6,54 @@ export const generateUserSettings = (parentElement, pubSub) => {
     
     return{
         render : function (settings){
+            settings
+            const html = `
+            <div class="columns">
+        <div class="column is-5">
 
+            <div class="group">
+                <div></div>
+                <h1 class="title"> Settings </h1>
+            </div>
+
+            <div class="field">
+                <label class="label">Username</label>
+                <div class="control">
+                    <input class="input" type="text" placeholder="Username"/><!-- da sostituire il placeholder -->
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label">Old password</label>
+                <div class="control">
+                    <input class="input" type="text" placeholder="password"/> <!-- da sostituire il placeholder -->
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label">New password</label>
+                <div class="control">
+                    <input class="input" type="text" placeholder="new password"/> <!-- da sostituire il placeholder -->
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label">Confirm password</label>
+                <div class="control">
+                    <input class="input" type="text" placeholder="new password"/> <!-- da sostituire il placeholder -->
+                </div>
+            </div>
+
+        </div>
+        <div class="column is-1">
+            2
+        </div>
+        <div class="column is-5">
+            <textarea class="textarea" placeholder="bio content"></textarea>
+        </div>
+    </div>
+            `;
+            parentElement.innerHTML = html;
         }
     }
 }
