eb12e0a48fc33143e90b4fbf931ed01d31276c96
Luca Avveduto <avvedutoluca@itis-molinari.eu>
2025-05-11 12:23:41 +0200

    fix

--- public/src/modules/presentation/publisher.js
+++ public/src/modules/presentation/publisher.js
@@ -1,8 +1,11 @@
 export const generatePublisher = (pubsub, document, view) => {
     view.render();
+    pubsub.subscribe("delete-draft", () => {
+        pubsub.publish("delete-document", document.getID());
+    });
     return{
         addTag: function(tag) {
-
+            
         },
         createNewTag: function(tag) {
 
