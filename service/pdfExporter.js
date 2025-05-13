6f5f169d27beaaef45047de4e9b4dd0c1ee911f6
AngeloRipamonti <ripamontiangelo@itis-molinari.eu>
2025-05-13 17:26:37 +0200

    wkhtmltopdf

--- service/pdfExporter.js
+++ service/pdfExporter.js
@@ -1,21 +1,15 @@
-const htmlPdf = require('html-pdf');
+const wkhtmltopdf = require('wkhtmltopdf');
+const fs = require('fs');
 const path = require('path');
 const [html, filename] = process.argv.slice(2);
 
-htmlPdf.create(html, {
-    format: 'A4',
-    border: {
-        top: '10mm',
-        right: '10mm',
-        bottom: '10mm',
-        left: '10mm'
-    }
-})
-.toFile(path.join(process.cwd(), `/dist/assets/pdf/${filename}`), (err, res) => {
-    if (err) {
+wkhtmltopdf(html, { pageSize: 'A4' })
+    .pipe(fs.createWriteStream(path.join(process.cwd(), 'dist/assets/pdf', filename)))
+    .on('finish', () => {
+        console.log(`/dist/assets/pdf/${filename}`);
+        process.exit(0);
+    })
+    .on('error', (err) => {
         console.log(err);
-        process.exit(1);   
-    }
-    console.log(`/dist/assets/pdf/${filename}`);
-    process.exit(0);
-});
+        process.exit(1);
+    });
